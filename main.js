21c230b160f6507fa2b848a37e7af0eac42413f2
Stas Evdok <75881037+stasevdok@users.noreply.github.com>
2023-12-27 20:48:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+let modal = document.getElementById("pop-up")
+document.getElementById("open-pop-up").addEventListener("click", () => {
+    modal.style.pointerEvents = "all";
+    modal.style.opacity = 1;
+
+})
+
+document.getElementById("hide-pop-up").addEventListener("click", () => {
+    modal.style.pointerEvents = "none";
+    modal.style.opacity = 0;
+})
+
+const accordionContent = document.querySelectorAll(".accordion-content");
+accordionContent.forEach((item, index) => {
+    let header = item.querySelector("header");
+    header.addEventListener("click", () =>{
+        item.classList.toggle("open");
+        let description = item.querySelector(".description");
+        if(item.classList.contains("open")){
+            description.style.height = `${description.scrollHeight}px`; 
+            item.querySelector("i").classList.replace("fa-plus", "fa-minus");
+        }else{
+            description.style.height = "0px";
+            item.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+        removeOpen(index); 
+    })
+})
+function removeOpen(index1){
+    accordionContent.forEach((item2, index2) => {
+        if(index1 != index2){
+            item2.classList.remove("open");
+            let des = item2.querySelector(".description");
+            des.style.height = "0px";
+            item2.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+    })
+}
